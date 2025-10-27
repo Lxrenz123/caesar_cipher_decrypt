c5531aee81bf9d2e810876abef264b1ae672c98a
Lorenz Ritsch <ritschlorenz14@gmail.com>
2025-10-27 08:51:37 +0200

    improved version, handling special characters and uppercase

--- caesar.py
+++ caesar.py
@@ -2,7 +2,7 @@
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 
 
-ciphertext = "fcwkgqrcp"
+ciphertext = "wpjvJAM{jhlzhy_k3jy9wa3k_86kl32k2}"
 
 decrypted = ""
 key = 1
@@ -12,10 +12,19 @@ while key <= 25:
     print(" KEY: " + str(key))   
   
     for j in ciphertext:
+        if j.isalpha():
 
-        index = alphabet.index(j)
-        decrypted += alphabet[(index - key) % len(alphabet)]
-        
+            a = alphabet.upper() if j.isupper() else alphabet.lower()
+            
+            index = a.index(j)
+       
+            decrypted += a[(index - key) % len(a)]
+
+        else:
+            
+            decrypted += j
+            
+            
     print(decrypted)
     decrypted = ""
     key += 1
